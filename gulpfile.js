4997643e9a5c85355eafbd6de9d34237797742a5
Yustynn Panicker <yustynn@gmail.com>
2016-11-08 20:48:59 +0800

    oops

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,23 @@ const gulp = require('gulp');
 
 // plugins
 const cssVip = require("gulp-css-vip"); // adds !important to all css styles
+const plumber = require("gulp-plumber"); // he keeps us running smooth
 const rename = require('gulp-rename'); // we want to change the output filename
 const watch = require('gulp-watch'); // auto-runs the 'gulp' command for you
 
 gulp.task( 'build', () => {
   return gulp.src('./src/styles.css')
+    .pipe(plumber({
+        errorHandler: function (err) {
+            console.log(err);
+            this.emit('end');
+        }
+    }))
     .pipe( cssVip() )
-    .pipe( rename('override-styles.css'))
+    .pipe( rename('override-styles.css') )
     .pipe( gulp.dest('./') )
 } );
 
-gulp.task( 'default', ['build'], () => {
-  // gulp.task('build');
+gulp.task( 'default', () => {
   return gulp.watch('./src/styles.css', ['build'])
 } );
